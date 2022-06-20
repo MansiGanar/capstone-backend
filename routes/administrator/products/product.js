022f4decbfe7a0a4f3a91e45371c2d337251a2d4
Mansi Ganar <86571822+MansiGanar@users.noreply.github.com>
2022-06-20 23:08:38 +0200

    log added

--- routes/administrator/products/product.js
+++ routes/administrator/products/product.js
@@ -77,6 +77,7 @@ router.post(
         });
       }
     } catch (error) {
+      console.log(error);
       res
         .status(400)
         .json({ msg: "Failed to add a new product. Please try again." });
